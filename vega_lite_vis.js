45881b206cd1b1e9632fcae8c429a43ed658ef34
Patrick <zxuu0075@student.monash.edu>
2023-10-08 13:26:28 +1100

    Added new chart for assignment2

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -12,3 +12,8 @@ var vg_3 = "donut_chart.vg.json";
 vegaEmbed("#donut_chart", vg_3).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_4 = "bar_chart.vg.json";
+vegaEmbed("#bar_chart", vg_4).then(function(result) {
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
