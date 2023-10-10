c4074bd57072c3c7c256f630477593c1a9556f76
Patrick <zxuu0075@student.monash.edu>
2023-10-10 22:03:56 +1100

    Addition: added one more chart for website

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -17,3 +17,8 @@ var vg_4 = "bar_chart.vg.json";
 vegaEmbed("#bar_chart", vg_4).then(function(result) {
     // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
+
+var vg_5 = "scatter_plot.vg.json";
+vegaEmbed("#scatter_plot", vg_5).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
